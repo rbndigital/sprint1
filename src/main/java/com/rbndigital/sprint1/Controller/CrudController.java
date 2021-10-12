2a2450c22c932c333d2586e2119fa688c52dd9a5
Alexander <alexandermriver@gmail.com>
2021-10-11 22:08:34 -0500

    Full_Migración

--- src/main/java/com/rbndigital/sprint1/Controller/CrudController.java
+++ src/main/java/com/rbndigital/sprint1/Controller/CrudController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import com.rbndigital.sprint1.Entidades.Curso;
 import com.rbndigital.sprint1.Servicios.CrudServicio;
 
@@ -14,8 +16,7 @@ public class CrudController {
 	private CrudServicio cursoService;
 	
 	@GetMapping("/siaG42")
-	public String seleccionarCursos(Model model)
-	{
+	public String seleccionarCursos(Model model){
 		try {
 			List<Curso> listaCursos = cursoService.findAll();
 			model.addAttribute("cursos", listaCursos);
@@ -26,4 +27,48 @@ public class CrudController {
 		return "index";
 	}
 
+	@RequestMapping("/estadisticas")
+	public ModelAndView estadisticas() {
+		return new ModelAndView("estadisticas");
+	}
+	@RequestMapping("/fajardo")
+	public ModelAndView fajardo() {
+		return new ModelAndView("fajardo");
+	}
+	@RequestMapping("/gaviria")
+	public ModelAndView gaviria() {
+		return new ModelAndView("gaviria");
+	}
+	@RequestMapping("/gutierrez")
+	public ModelAndView gutierrez() {
+		return new ModelAndView("gutierrez");
+	}
+	@RequestMapping("/mapa")
+	public ModelAndView mapa() {
+		return new ModelAndView("mapa");
+	}
+	@RequestMapping("/marquez")
+	public ModelAndView marquez() {
+		return new ModelAndView("marquez");
+	}
+	@RequestMapping("/petro")
+	public ModelAndView petro() {
+		return new ModelAndView("petro");
+	}
+	@RequestMapping("/registro")
+	public ModelAndView registro() {
+		return new ModelAndView("registro");
+	}
+	@RequestMapping("/Votación")
+	public ModelAndView Votación() {
+		return new ModelAndView("Votación");
+	}
+	@RequestMapping("/zuluaga")
+	public ModelAndView zuluaga() {
+		return new ModelAndView("zuluaga");
+	}
+	@RequestMapping("/acercaDe")
+	public ModelAndView acercaDe() {
+		return new ModelAndView("acercaDe");
+	}
 }
