43af43f7fb2538b811f3ac9300b8ab4be441bf53
Alexander <alexandermriver@gmail.com>
2021-10-15 07:01:41 -0500

    ModificarVotantes

--- src/main/java/com/rbndigital/sprint1/Controller/CrudController.java
+++ src/main/java/com/rbndigital/sprint1/Controller/CrudController.java
@@ -1,10 +1,12 @@
 package com.rbndigital.sprint1.Controller;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -42,6 +44,15 @@ public class CrudController {
 		crudservicio.save(votantes);
 		return "redirect:/votantes";
 	}
+
+	@GetMapping("/modVotantes/{id}")
+		public String Editar(@PathVariable int id,Model model){
+			Optional<Votantes> votantes = crudservicio.findById(id);
+			model.addAttribute("votantes",votantes.get());
+			return "/modVotantes";
+		}
+	
+
 
 
 	@RequestMapping("/index")
